d9fbde773a4dce27b82ed5c4bf031f62a16387ad
juliakohn <jpk246@cornell.edu>
2023-10-30 11:19:02 -0400

    Update

--- Lab11/script.js
+++ Lab11/script.js
@@ -1,1 +1,12 @@
 
+
+mapboxgl.accessToken = 'pk.eyJ1IjoianVsaWFrb2huIiwiYSI6ImNsbjF3OXoxMjAydHQyemtocGxscHNmZXYifQ.C99bUGi2aZupJ_bvjaFrRA'
+
+const map = new mapboxgl.Map({
+	container: 'julia'
+	styles: 'mapbox://styles/juliakohn/clod1ni8j00sh01p6eqhkdgcb'
+	center: [-122.4194, 37.7749]
+	zoom: 12
+	pitch: 30
+	bearing: 0
+})
