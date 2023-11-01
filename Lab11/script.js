aff3460be0539b0a9cbebd9b483b0bf4c39e2766
juliakohn <jpk246@cornell.edu>
2023-11-01 11:24:57 -0400

    Update script.js

--- Lab11/script.js
+++ Lab11/script.js
@@ -5,14 +5,14 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianVsaWFrb2huIiwiYSI6ImNsbjF3OXoxMjAydHQyemtoc
 const map = new mapboxgl.Map({
 	container: 'julia'
 	style: 'mapbox://styles/juliakohn/clod1ni8j00sh01p6eqhkdgcb',
-	center: [-122.4194, 37.7749],
+	center: [-122.419, 37.774],
 	zoom: 12,
 	pitch: 30,
 	bearing: 0
 });
 
 map.on('load',function() {
-	console.log('my name is julia');
+	//console.log("my name is julia");
 
 	map.addSource('restaurantSource',{
 		'type': 'vector',
@@ -28,5 +28,11 @@ map.on('load',function() {
 			'circle-color': '#000f8c'
 		}
 	})
+
+		map.on('mouseenter', 'restaurantLayer', (e) => {
+			//console.log(e.features[0]{'[properties']['business_name']);
+			var name = e.features[0]['properties']['business_name'];
+		})
+
 
 }
