4921b0605ce09e2bc4e270ae28ec2e31a8e9694d
juliakohn <jpk246@cornell.edu>
2023-11-01 10:24:51 -0400

    Update

--- Lab11/script.js
+++ Lab11/script.js
@@ -4,9 +4,27 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianVsaWFrb2huIiwiYSI6ImNsbjF3OXoxMjAydHQyemtoc
 
 const map = new mapboxgl.Map({
 	container: 'julia'
-	styles: 'mapbox://styles/juliakohn/clod1ni8j00sh01p6eqhkdgcb'
+	styles: 'mmapbox://styles/juliakohn/clod1ni8j00sh01p6eqhkdgcb'
 	center: [-122.4194, 37.7749]
 	zoom: 12
 	pitch: 30
 	bearing: 0
 })
+
+map.on('load', function() {
+	map.addSource('restaurantSource',{
+		'type': 'vector',
+		'url': ''
+	});
+
+	map.addLayer({
+		'id': 'restaurantLayer',
+		'type': 'circle',
+		'source': 'restaurantSource',
+		'source-layer': 'Restaurant_Scores_-_LIVES_Sta-3hd45t',
+		'paint': {
+			'circle-color': '#063f8c'
+		}
+	})
+
+}
